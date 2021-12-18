87f44d5f0e7f9f7a855e6fd82d3424905556a9bd
Oxidised-Man <72887386+Oxidisedman@users.noreply.github.com>
2021-12-18 17:58:17 +0530

    Fixed wroking issues

--- bot.py
+++ bot.py
@@ -43,6 +43,16 @@ oxi = bot.start(bot_token=TOKEN)
 
 
 # fake data generator
+async def is_admin(event, user):
+    try:
+        sed = await event.client.get_permissions(event.chat_id, user)
+        if sed.is_admin:
+            is_mod = True
+        else:
+            is_mod = False
+    except:
+        is_mod = False
+    return is_mod
 
 @oxi.on(events.NewMessage(pattern="^[!?/]gen"))
 async def hi(event):
