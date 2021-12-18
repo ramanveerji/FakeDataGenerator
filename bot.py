c32cd94572414477f1449cce418bcd1a46e1d169
Oxidised-Man <72887386+Oxidisedman@users.noreply.github.com>
2021-12-18 15:27:54 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,40 @@
+import asyncio
+import requests
+import os
+import logging
+import time
+import string
+import random
+import html
+
+# telethon
+from telethon import events, Button, TelegramClient
+
+from telethon.tl import functions, types
+
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+from telethon.utils import get_input_location
+
+logging.basicConfig(level=logging.INFO)
+
+try:
+    API_ID = int(os.environ.get("API_ID", 6))
+    API_HASH = os.environ.get("API_HASH", None)
+    TOKEN = os.environ.get("TOKEN", None)
+except ValueError:
+    print("You forgot to fullfill vars")
+    print("Bot is quitting....")
+    exit()
+except Exception as e:
+    print(f"Error - {str(e)}")
+    print("Bot is quitting.....")
+    exit()
+except ApiIdInvalidError:
+    print("Your API_ID or API_HASH is Invalid.")
+    print("Bot is quitting.")
+    exit()
+
+bot = TelegramClient('bin', API_ID, API_HASH)
+bin = bot.start(bot_token=TOKEN)
